d1a12953b0ee10c69a550fa1c1eba00f29351475
BrightStar <brightstar09031222@gmail.com>
2024-02-17 15:54:07 +1000

    Hash Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
@@ -10,9 +10,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter>
         <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
   // </React.StrictMode>
 );
