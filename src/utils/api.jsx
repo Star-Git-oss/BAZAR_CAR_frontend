67829b73cbfdfc8f37b8fd22155e1f665c75d087
BrightStar <brightstar09031222@gmail.com>
2024-02-23 00:38:25 +1000

    frontend BACKEND_URI

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,5 +1,5 @@
-
-export const BACKEND_URI = 'http://localhost:5005/';
+// export const BACKEND_URI = 'http://localhost:5005/';
+export const BACKEND_URI = 'jolly-curious-baboon.ngrok-free.app/';
 
 export const UPLOAD_URI = BACKEND_URI + 'uploads/';
 export const BASE_URI = BACKEND_URI + 'api';
