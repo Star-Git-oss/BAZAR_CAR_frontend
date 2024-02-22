9dacb71f7586e405f2089026636854063305528f
BrightStar <brightstar09031222@gmail.com>
2024-02-23 01:06:11 +1000

    frontend bug

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,5 @@
-import React from "react";
-
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
